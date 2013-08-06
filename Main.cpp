af2ef926a8439569f7b5fdb4921b40d9e9604cb4
Rafal Romanowski <wegdrasil@gmail.com>
2013-08-06 20:20:59 +0200

    Updated Main.

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,4 @@
 #include <Windows.h>
-#include "QuadDemo.h"
-
 
 void Loop()
 {
@@ -21,16 +19,5 @@ void Loop()
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 { 
-	//Window window(hInstance, &settings);
-	
-	QuadDemo* demo = new QuadDemo();
-	//demo->Go(;)
-	demo->m_pWindowSettings->m_bFullScreen = true;
-	demo->m_pWindowSettings->m_bMSAAEnable = false;
-	demo->m_pWindowSettings->m_iHeight = 600;
-	demo->m_pWindowSettings->m_iWidth = 800;
-	demo->m_pWindowSettings->m_sCaption = L"eFrame";
-		 
-	Loop();
-	
+		
 }
