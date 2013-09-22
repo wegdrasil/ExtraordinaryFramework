13c22100a75ea1eb7dae6d2ac591b01b3823ee40
Rafal Romanowski <wegdrasil@gmail.com>
2013-09-22 14:51:39 +0200

    Cleaned repository

--- QuadVertexShader.hlsl
+++
@@ -1,9 +0,0 @@
-cbuffer cbPerObject 
-{
-	float4x4 WorldViewProj;
-};
-
-void VS(float3 iPos : POSITION, float3 iNormal : NORMAL, float2 iTex : TEXCOORD, out float4 oPosH : SV_POSITION)
-{
-	oPosH = mul(float4(iPos, 1.0f), WorldViewProj);
-}
--- Source/Shaders/VecAddComputeShader.hlsl
+++ Source/Shaders/VecAddComputeShader.hlsl
@@ -7,7 +7,7 @@ StructuredBuffer<Data> inputA;
 StructuredBuffer<Data> inputB;
 RWStructuredBuffer<Data> output;
 
-[numthreads(64, 1, 1)]
+[numthreads(256, 1, 1)]
 void main( uint3 DTid : SV_DispatchThreadID )
 {
 	output[DTid.x].vec = inputA[DTid.x].vec + inputB[DTid.x].vec;	
